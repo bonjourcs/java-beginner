edc9969cdc5498aa04d0f96445719f14e2b18056
Liang Chenghao <lch_office@foxmail.com>
2018-04-03 17:09:19 +0800

    feat(generics): add more subtype demo code.

---
+++ learn-the-java-language/src/main/java/com/bonjourcs/java/ltjl/generics/Pineapple.java
@@ -0,0 +1,9 @@
+package com.bonjourcs.java.ltjl.generics;
+
+/**
+ * @author Liang Chenghao
+ * Description: class Pineapple extends Fruit
+ * Date: 2018/4/3
+ */
+public class Pineapple extends Fruit {
+}
---
+++ learn-the-java-language/src/test/java/com/bonjourcs/java/ltjl/generics/SubtypeTest.java
@@ -0,0 +1,35 @@
+package com.bonjourcs.java.ltjl.generics;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Liang Chenghao
+ * Description:
+ * Date: 2018/4/3
+ */
+public class SubtypeTest {
+
+    @Test
+    public void testGenericSubtype() {
+
+        // valid
+        List<? super Fruit> list = new ArrayList<>();
+        list.add(new Fruit());
+        list.add(new Apple());
+        list.add(new Pineapple());
+
+        Pineapple pineapple = (Pineapple) list.get(2);
+
+        // valid
+        List<? super Pineapple> list1 = new ArrayList<Fruit>();
+        // invalid
+//        list1.add(new Fruit());
+        list1.add(new Pineapple());
+
+
+    }
+
+}
