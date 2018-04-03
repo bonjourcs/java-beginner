50bad7fd4b30a9d7eb52e89ef1795a00c9c081d0
Liang Chenghao <lch_office@foxmail.com>
2018-04-03 17:11:39 +0800

    feat(generics): add more subtype demo code.

---
+++ learn-the-java-language/src/main/java/com/bonjourcs/java/ltjl/generics/Apple.java
@@ -0,0 +1,9 @@
+package com.bonjourcs.java.ltjl.generics;
+
+/**
+ * @author Liang Chenghao
+ * Description: class Apple extends Fruit
+ * Date: 2018/4/3
+ */
+public class Apple extends Fruit {
+}
---
+++ learn-the-java-language/src/main/java/com/bonjourcs/java/ltjl/generics/Fruit.java
@@ -0,0 +1,9 @@
+package com.bonjourcs.java.ltjl.generics;
+
+/**
+ * @author Liang Chenghao
+ * Description: class Fruit
+ * Date: 2018/4/3
+ */
+public class Fruit {
+}
