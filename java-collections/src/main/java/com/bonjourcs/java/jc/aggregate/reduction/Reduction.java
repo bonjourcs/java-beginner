8ca3961676537815fb450373efa91e92a284f92c
Liang Chenghao <lch_office@foxmail.com>
2018-05-10 17:02:29 +0800

    feat(jc): add reduction operation test.

---
+++ java-collections/src/main/java/com/bonjourcs/java/jc/aggregate/reduction/Reduction.java
@@ -0,0 +1,10 @@
+package com.bonjourcs.java.jc.aggregate.reduction;
+
+/**
+ * @author Liang Chenghao
+ * Description: for reduction operation
+ * Date: 2018/5/10
+ */
+public class Reduction {
+
+}
---
+++ java-collections/src/test/java/com/bonjourcs/java/jc/aggregate/reduction/ReductionTest.java
@@ -0,0 +1,35 @@
+package com.bonjourcs.java.jc.aggregate.reduction;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+/**
+ * @author Liang Chenghao
+ * Description: reduction test
+ * Date: 2018/5/10
+ */
+public class ReductionTest {
+
+    /**
+     * test max reduction
+     */
+    @Test
+    public void testMax() {
+        Assert.assertTrue(!"".equals(Stream.of("Hello", "World", "This", "is")
+                .max(String::compareToIgnoreCase)
+                .orElse("")));
+    }
+
+    /**
+     * test findAny reduction
+     */
+    @Test
+    public void testFindAny() {
+        Assert.assertTrue(
+                Stream.of("Hi", "There", "Ok").anyMatch(s -> s.startsWith("H"))
+        );
+    }
+
+}
