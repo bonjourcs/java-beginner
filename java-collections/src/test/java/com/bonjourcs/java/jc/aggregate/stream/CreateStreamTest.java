3bdbcbeae1bb28b219bb22f9420ba4f1d7713678
Liang Chenghao <lch_office@foxmail.com>
2018-05-10 15:13:02 +0800

    feat(jc): add map operation.

--- java-collections/src/test/java/com/bonjourcs/java/jc/aggregate/stream/CreateStreamTest.java
+++ java-collections/src/test/java/com/bonjourcs/java/jc/aggregate/stream/CreateStreamTest.java
@@ -2,7 +2,7 @@ package com.bonjourcs.java.jc.aggregate.stream;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.stream.Stream;
 
 /**
  * @author Liang Chenghao
@@ -16,6 +16,14 @@ public class CreateStreamTest {
         CreateStream createStream = new CreateStream();
         createStream.showStream("show double stream",
                 createStream.generateStream(), 10);
+    }
+
+    @Test
+    public void testShowToUpperStringStream() {
+        Stream<String> stringStream = Stream
+                .of("Hello", "World", "I", "Love", "This", "World");
+        // use map operate
+        new CreateStream().showStream("upper string stream", stringStream.map(String::toUpperCase), 10);
     }
 
 }
