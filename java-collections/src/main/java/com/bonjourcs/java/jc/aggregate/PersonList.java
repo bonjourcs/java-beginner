f3822e5f2b659eab113bb6763949138db296f4a8
Liang Chenghao <lch_office@foxmail.com>
2018-05-15 15:33:15 +0800

    update(jc): use more effective reduce operation.

--- java-collections/src/main/java/com/bonjourcs/java/jc/aggregate/PersonList.java
+++ java-collections/src/main/java/com/bonjourcs/java/jc/aggregate/PersonList.java
@@ -12,103 +12,103 @@ import java.util.stream.Collectors;
  */
 public class PersonList {
 
-    private List<Person> people = new ArrayList<>();
+	private List<Person> people = new ArrayList<>();
 
-    public void add(Person person) {
-        people.add(person);
-    }
+	public void add(Person person) {
+		people.add(person);
+	}
 
-    /**
-     * show each person's name
-     */
-    public void printPeronName() {
-        people.forEach(e -> System.out.println(e.getName()));
-    }
+	/**
+	 * show each person's name
+	 */
+	public void printPeronName() {
+		people.forEach(e -> System.out.println(e.getName()));
+	}
 
-    /**
-     * only show male's name
-     */
-    public void printMaleName() {
-        people.stream()
-                .filter(e -> e.getSex() == Person.Sex.MALE)
-                .forEach(e -> System.out.println(e.getName()));
-    }
+	/**
+	 * only show male's name
+	 */
+	public void printMaleName() {
+		people.stream()
+				.filter(e -> e.getSex() == Person.Sex.MALE)
+				.forEach(e -> System.out.println(e.getName()));
+	}
 
-    /**
-     * get average age of male people
-     *
-     * @return average age
-     */
-    public double getMaleAverageAge() {
-        return people.stream()
-                .filter(e -> e.getSex() == Person.Sex.MALE)
-                .mapToInt(Person::getAge)
-                .average()
-                .getAsDouble();
-    }
+	/**
+	 * get average age of male people
+	 *
+	 * @return average age
+	 */
+	public double getMaleAverageAge() {
+		return people.stream()
+				.filter(e -> e.getSex() == Person.Sex.MALE)
+				.mapToInt(Person::getAge)
+				.average()
+				.getAsDouble();
+	}
 
-    /**
-     * get total age of person
-     *
-     * @return total age
-     */
-    public int getTotalAge() {
-        return people.stream()
-                .map(Person::getAge)
-                .reduce(0, (a, b) -> a + b);
-    }
+	/**
+	 * get total age of person
+	 *
+	 * @return total age
+	 */
+	public int getTotalAge() {
+		return people.stream()
+				.map(Person::getAge)
+				.reduce(0, Integer::sum);
+	}
 
-    /**
-     * get average age
-     *
-     * @return average age
-     */
-    public double getAverageAge() {
-        return people.stream()
-                .map(Person::getAge)
-                .collect(Averager::new, Averager::accept, Averager::combine)
-                .average();
-    }
+	/**
+	 * get average age
+	 *
+	 * @return average age
+	 */
+	public double getAverageAge() {
+		return people.stream()
+				.map(Person::getAge)
+				.collect(Averager::new, Averager::accept, Averager::combine)
+				.average();
+	}
 
-    /**
-     * sort by age
-     *
-     * @return map
-     */
-    public Map<Person.Sex, List<Person>> sortByGender() {
-        return people.stream()
-                .collect(Collectors.groupingBy(Person::getSex));
-    }
+	/**
+	 * sort by age
+	 *
+	 * @return map
+	 */
+	public Map<Person.Sex, List<Person>> sortByGender() {
+		return people.stream()
+				.collect(Collectors.groupingBy(Person::getSex));
+	}
 
-    /**
-     * sort name by age
-     *
-     * @return map
-     */
-    public Map<Person.Sex, List<String>> namesByGender() {
-        return people.stream()
-                .collect(
-                        Collectors.groupingBy(
-                                Person::getSex,
-                                Collectors.mapping(
-                                        Person::getName,
-                                        Collectors.toList()
-                                )
-                        )
-                );
-    }
+	/**
+	 * sort name by age
+	 *
+	 * @return map
+	 */
+	public Map<Person.Sex, List<String>> namesByGender() {
+		return people.stream()
+				.collect(
+						Collectors.groupingBy(
+								Person::getSex,
+								Collectors.mapping(
+										Person::getName,
+										Collectors.toList()
+								)
+						)
+				);
+	}
 
-    /**
-     * get total age by gender
-     *
-     * @return map
-     */
-    public Map<Person.Sex, Integer> totalAgeByGender() {
-        return people.stream()
-                .collect(Collectors.groupingBy(
-                        Person::getSex,
-                        Collectors.reducing(0, Person::getAge, Integer::sum)
-                ));
-    }
+	/**
+	 * get total age by gender
+	 *
+	 * @return map
+	 */
+	public Map<Person.Sex, Integer> totalAgeByGender() {
+		return people.stream()
+				.collect(Collectors.groupingBy(
+						Person::getSex,
+						Collectors.reducing(0, Person::getAge, Integer::sum)
+				));
+	}
 
 }
