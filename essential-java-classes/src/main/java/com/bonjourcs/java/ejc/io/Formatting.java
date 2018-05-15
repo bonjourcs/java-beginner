2ab494323e2340eaf0964695ec9e34e8c158f4fc
Liang Chenghao <lch_office@foxmail.com>
2018-05-15 16:59:34 +0800

    feat(io): add formatting code.

---
+++ essential-java-classes/src/main/java/com/bonjourcs/java/ejc/io/Formatting.java
@@ -0,0 +1,14 @@
+package com.bonjourcs.java.ejc.io;
+
+/**
+ * @author Liang Chenghao
+ * Description: for formatting string
+ * Date: 2018/5/15
+ */
+public class Formatting {
+
+	public static void main(String[] args) {
+		System.out.format("%f :  %1$+020.10f %n", Math.PI);
+	}
+
+}
