2702721f68422ab26028b394df3cfac69fa693de
Liang Chenghao <lch_office@foxmail.com>
2018-05-15 15:58:34 +0800

    update(test): add primitive type stream code.

--- java-collections/src/test/java/com/bonjourcs/java/jc/aggregate/stream/CreateStreamTest.java
+++ java-collections/src/test/java/com/bonjourcs/java/jc/aggregate/stream/CreateStreamTest.java
@@ -1,8 +1,10 @@
 package com.bonjourcs.java.jc.aggregate.stream;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 /**
@@ -12,29 +14,40 @@ import java.util.stream.Stream;
  */
 public class CreateStreamTest {
 
-    @Test
-    public void testShowStream() {
-        CreateStream createStream = new CreateStream();
-        createStream.showStream("show double stream",
-                createStream.generateStream(), 10);
-    }
+	@Test
+	public void testShowStream() {
+		CreateStream createStream = new CreateStream();
+		createStream.showStream("show double stream",
+				createStream.generateStream(), 10);
+	}
 
-    @Test
-    public void testShowToUpperStringStream() {
-        Stream<String> stringStream = Stream
-                .of("Hello", "World", "I", "Love", "This", "World");
-        // use map operate
-        new CreateStream().showStream("upper string stream", stringStream.map(String::toUpperCase), 10);
-    }
+	@Test
+	public void testShowToUpperStringStream() {
+		Stream<String> stringStream = Stream
+				.of("Hello", "World", "I", "Love", "This", "World");
+		// use map operate
+		new CreateStream().showStream("upper string stream", stringStream.map(String::toUpperCase), 10);
+	}
 
-    @Test
-    public void testIterateStream() {
+	@Test
+	public void testIterateStream() {
 
-        // reduction operation will generate a list
-        Arrays.asList(Stream.iterate(1, p -> p * 2)
-                .peek(e -> System.out.println("generate element : " + e))
-                .limit(10)
-                .toArray())
-                .forEach(System.out::println);
-    }
+		// reduction operation will generate a list
+		Arrays.asList(Stream.iterate(1, p -> p * 2)
+				.peek(e -> System.out.println("generate element : " + e))
+				.limit(10)
+				.toArray())
+				.forEach(System.out::println);
+	}
+
+	/**
+	 * resolve to primitive type stream
+	 */
+	@Test
+	public void testGeneratePrimitiveTypeStream() {
+		// generate a LongStream
+		LongStream longStream = LongStream.of(10L, 20L, 30L, 40L, 50L);
+		Assert.assertEquals(5, longStream.toArray().length);
+	}
+
 }
