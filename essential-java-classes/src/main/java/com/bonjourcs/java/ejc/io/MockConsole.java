fce584af4628702aab83a324277191e8811272ef
Liang Chenghao <lch_office@foxmail.com>
2018-05-15 18:50:12 +0800

    update(io): update command line i/o code.

--- essential-java-classes/src/main/java/com/bonjourcs/java/ejc/io/MockConsole.java
+++ essential-java-classes/src/main/java/com/bonjourcs/java/ejc/io/MockConsole.java
@@ -14,13 +14,19 @@ public class MockConsole {
 	private static final String CLEAR = "clear";
 	private static final String SU = "su";
 	private static final int ROW = 5;
+	private static final Scanner in = new Scanner(System.in);
+	private static final String ROOT_PASSWORD = "ROOT";
 
 	public static void main(String[] args) {
 
 		// show login banner
 		login();
 
-		Scanner in = new Scanner(System.in);
+		preNormal();
+
+	}
+
+	private static void preNormal() {
 		System.out.print("[bonjourcs@localhost ~]$ ");
 
 		String str;
@@ -28,7 +34,6 @@ public class MockConsole {
 			// handle input string
 			handle(str);
 		}
-
 	}
 
 	/**
@@ -75,6 +80,8 @@ public class MockConsole {
 			logout();
 		} else if (CLEAR.equalsIgnoreCase(str)) {
 			clear();
+		} else if (SU.equalsIgnoreCase(str)) {
+			su();
 		} else {
 			normal(str);
 		}
@@ -99,6 +106,69 @@ public class MockConsole {
 			System.out.println();
 		}
 		System.out.print("[bonjourcs@localhost ~]$ ");
+	}
+
+	/**
+	 * su
+	 */
+	private static void su() {
+		System.out.print("Password: ");
+		String str = in.next();
+
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			// do nothing
+		}
+
+		if (ROOT_PASSWORD.equals(str)) {
+			System.out.println("su: Authentication failure.");
+			preNormal();
+		} else {
+			preSuNormal();
+		}
+	}
+
+	/**
+	 * root user loop condition
+	 */
+	private static void preSuNormal() {
+		String str;
+		System.out.print("[root@localhost /home/root]# ");
+		while ((str = in.next()) != null) {
+			suHandleString(str);
+		}
+	}
+
+	/**
+	 * root user handle input string
+	 *
+	 * @param str
+	 */
+	private static void suHandleString(String str) {
+		if (EXIT.equalsIgnoreCase(str)) {
+			suLogout();
+		} else {
+			suNormal(str);
+		}
+	}
+
+	/**
+	 * root user exit
+	 */
+	private static void suLogout() {
+		System.out.println("exit");
+		preNormal();
+	}
+
+	/**
+	 * handle another operation
+	 *
+	 * @param str input string
+	 */
+	private static void suNormal(String str) {
+		System.out.println("-bash: " + str + ": command not found");
+		System.out.print("[root@localhost /home/root]# ");
 	}
 
 }
