ac26fcf29ba425730bdc0473b5fbe02d96ade4e0
Liang Chenghao <lch_office@foxmail.com>
2018-05-15 16:34:29 +0800

    update(io): format code.

--- essential-java-classes/src/main/java/com/bonjourcs/java/ejc/io/ScanFile.java
+++ essential-java-classes/src/main/java/com/bonjourcs/java/ejc/io/ScanFile.java
@@ -10,63 +10,68 @@ import java.util.Scanner;
  */
 public class ScanFile {
 
-    // get classpath
-    private static final String path = ClassLoader.getSystemResource("").getPath();
+	/**
+	 * get classpath
+	 */
+	private static final String path = ClassLoader.getSystemResource("").getPath();
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        // get all content
-        scanFile("file_read.txt");
+		// get all content
+		scanFile("file_read.txt");
 
-        // get only numbers
-        scanNumber("file_with_number.txt");
+		// get only numbers
+		scanNumber("file_with_number.txt");
 
-    }
+	}
 
-    /**
-     * scan file content
-     * @param fileName file to scan
-     * @throws IOException exceptions
-     */
-    private static void scanFile(String fileName) throws IOException {
+	/**
+	 * scan file content
+	 *
+	 * @param fileName file to scan
+	 * @throws IOException exceptions
+	 */
+	private static void scanFile(String fileName) throws IOException {
 
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(new BufferedReader(new FileReader(path + fileName)));
-            while (scanner.hasNext()) {
-                System.out.println(scanner.next());
-            }
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
-        }
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new BufferedReader(new FileReader(path + fileName)));
 
-    }
+			while (scanner.hasNext()) {
+				System.out.println(scanner.next());
+			}
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+
+	}
 
 
-    /**
-     * scan only integers
-     * @param fileName file to scan
-     * @throws IOException exceptions
-     */
-    private static void scanNumber(String fileName) throws IOException {
+	/**
+	 * scan only integers
+	 *
+	 * @param fileName file to scan
+	 * @throws IOException exceptions
+	 */
+	private static void scanNumber(String fileName) throws IOException {
 
-        Scanner scanner = null;
+		Scanner scanner = null;
 
-        try {
-            scanner = new Scanner(new BufferedReader(new FileReader(path + fileName)));
-            while (scanner.hasNext()) {
-                if (scanner.hasNextInt()) {
-                    System.out.println(scanner.nextInt());
-                } else {
-                    scanner.next();
-                }
-            }
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
-        }
-    }
+		try {
+			scanner = new Scanner(new BufferedReader(new FileReader(path + fileName)));
+			while (scanner.hasNext()) {
+				if (scanner.hasNextInt()) {
+					System.out.println(scanner.nextInt());
+				} else {
+					scanner.next();
+				}
+			}
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
+		}
+	}
 }
