12e8b6828a387bb55af235fce75c513adbd812ee
Liang Chenghao <lch_office@foxmail.com>
2018-12-25 13:54:12 +0800

    feat(j8f): use distinct method in stream.

--- java-8-feature/src/main/java/com/bonjourcs/java/java8/stream/StreamUp.java
+++ java-8-feature/src/main/java/com/bonjourcs/java/java8/stream/StreamUp.java
@@ -1,6 +1,9 @@
 package com.bonjourcs.java.java8.stream;
 
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -12,6 +15,15 @@ public class StreamUp {
 
     public static <T> void consumeStream(Stream<T> stream, Consumer<T> consumer) {
         stream.forEach(consumer);
+    }
+
+    public static <T> List<T> filterUnique(List<T> list, Predicate<T> predicate) {
+
+        return list.stream()
+                .filter(predicate)
+                .distinct()
+                .collect(Collectors.toList());
+
     }
 
 }
--- java-8-feature/src/test/java/com/bonjourcs/java/java8/stream/StreamUpTest.java
+++ java-8-feature/src/test/java/com/bonjourcs/java/java8/stream/StreamUpTest.java
@@ -1,10 +1,12 @@
 package com.bonjourcs.java.java8.stream;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /**
@@ -29,6 +31,15 @@ public class StreamUpTest {
         // stream can only action once
         StreamUp.consumeStream(stream, System.out::println);
         StreamUp.consumeStream(stream, System.out::println);
+
+    }
+
+    @Test
+    public void testFilterUnique() {
+
+        strings = Arrays.asList("Hello", "Hello", "World", "Am");
+        Assert.assertEquals(2,
+                StreamUp.filterUnique(strings, s -> s != null && s.length() > 3).size());
 
     }
 
