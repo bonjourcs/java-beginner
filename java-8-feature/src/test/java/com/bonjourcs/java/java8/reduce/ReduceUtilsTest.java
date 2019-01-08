dbfdabb10c74414a2b469944839ab3707faa7210
Liang Chenghao <lch_office@foxmail.com>
2019-01-08 14:35:07 +0800

    update(j8f): add stream demo.

--- java-8-feature/src/test/java/com/bonjourcs/java/java8/reduce/ReduceUtilsTest.java
+++ java-8-feature/src/test/java/com/bonjourcs/java/java8/reduce/ReduceUtilsTest.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -54,6 +56,19 @@ public class ReduceUtilsTest {
         // calculate total calories
         Assert.assertEquals(280, fishes.stream().mapToInt(Fish::getCalories).sum());
 
+    }
+
+    @Test
+    public void testGeneratePythagoreanTriples() {
+        getPythagoreanTriple().limit(5)
+                .forEach(t -> System.out.println(t[0] + "," + t[1] + "," + t[2]));
+    }
+
+    private Stream<int[]> getPythagoreanTriple() {
+        return IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                        .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
     }
 
 }
