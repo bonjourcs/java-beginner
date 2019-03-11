28d7295fd2dba0aec2b7c2c6de5ba5a9551ea618
Liang Chenghao <lch_office@foxmail.com>
2019-03-11 17:37:35 +0800

    feat(j8f): add collector demo.

---
+++ java-8-feature/src/main/java/com/bonjourcs/java/java8/collect/ToListCollector.java
@@ -0,0 +1,50 @@
+package com.bonjourcs.java.java8.collect;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+import static java.util.stream.Collector.Characteristics.*;
+
+/**
+ * @author Liang Chenghao
+ * Description: collector demo
+ * Date: 2019/3/11
+ */
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+
+    @Override
+    public Supplier<List<T>> supplier() {
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return List::add;
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (right, left) -> {
+            right.addAll(left);
+            return right;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(
+                UNORDERED,
+                CONCURRENT,
+                IDENTITY_FINISH
+        ));
+    }
+}
